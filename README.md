7c28613305b2ea7d7d65c7647fe6e17ed596dd0d
Jenna Vuong <jennavuong@gmail.com>
2016-03-22 17:30:14 -0700

    add new files

---
+++ README.md
@@ -0,0 +1,5 @@
+# react-webpack-redux-poc
+a poc for learning react, webpack, redux, es6
+
+npm install webpack --save -g
+npm install -save css-loader style-loader
---
+++ src/entry.js
@@ -0,0 +1,5 @@
+// 1st file, open index.html in browser to this work
+document.write("It works.");
+
+// 2nd file, included in index.html
+document.write(require("./content.js"));
